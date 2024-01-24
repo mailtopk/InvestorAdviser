4927c66fdb05ebf69b16c1b3fb5b7ec9c1cb3739
mailtopk <ppkumar.email@gmail.com>
2024-01-23 23:00:45 -0800

    added, nginx, docker support

--- src/svcinvest/main.py
+++ src/svcinvest/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, APIRouter
 from typing import Union
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI()
-@app.get("/")
+router = APIRouter()
+
+@router.get("/", tags=["Healthcheck"])
 def read_root():
+    print('in default')
     return {"Hello": "world"}
 
+@router.get("/stock", tags=["Stock"])
+def read_root(request: Request):
+    print('in test path ' + request.scope.get("root_path"))
+    return {"Hello": "world"}
+
+app.include_router(router)
